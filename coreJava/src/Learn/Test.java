53dcdf2ab383b7b4df36f18ec7e94f2181d6e60f
C <viranidhruv002@gmail.com>
2023-05-23 16:47:14 +0530

    Done

---
+++ coreJava/src/Learn/Test.java
@@ -0,0 +1,56 @@
+package Learn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+	public static void main(String[] args) {
+		// Q.1
+
+//		int i = 20+ +9- -12+ +4- -13+ +19;
+//		System.out.println(i);
+
+		// Q.2
+
+//		String s1 = "java";
+//		String s2 = "java";
+//		StringBuilder sb1 = new StringBuilder();
+//		sb1.append("ja").append("va");
+//		System.out.println(s1 == s2);
+//		System.out.println(s1.equals(s2));
+//		System.out.println(sb1.toString() == s1);
+//		System.out.println(sb1.toString().equals(s1));
+
+		
+		//Q.3
+		
+//		System.out.println("a");
+//		try {
+//			System.out.println("b");
+//			throw new IllegalArgumentException();
+//		}
+//		catch (RuntimeException e) {
+//			System.out.println("c");
+//		}
+//		finally {
+//			System.out.println("d");
+//		}
+//		System.out.println("e");
+		
+		
+		//Q.4
+		
+//		int[] array = {6,11,8};
+//		List<Integer> list = new ArrayList<Integer>();
+//		list.add(array[0]);
+//		list.add(array[2]);
+//		list.set(1, array[1]);
+//		list.remove(0);
+//		System.out.println(list);
+		
+		
+		//Q.4
+		
+		
+	}
+}
