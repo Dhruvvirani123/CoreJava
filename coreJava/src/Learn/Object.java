b27d2eedb3dd1e1baf123ae87846c26d3e8d1cb2
C <viranidhruv002@gmail.com>
2023-06-01 21:55:37 +0530

    Done

---
+++ coreJava/src/Learn/Object.java
@@ -0,0 +1,13 @@
+package Learn;
+
+class obj {
+	int x = 10, y = 20;
+}
+
+public class Object {
+
+	public static void main(String[] args) {
+		obj o = new obj();
+		System.out.println("Object : " + o.x + " Object : " + o.y);
+	}
+}
