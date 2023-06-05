edc60c693328c1c1612fb01b9835f1b00645efc5
C <viranidhruv002@gmail.com>
2023-06-05 22:18:00 +0530

    Done

---
+++ coreJava/src/Learn/This.java
@@ -0,0 +1,26 @@
+package Learn;
+
+class student {
+	int rollno;
+	String name;
+	float fee;
+
+	student(int rollno, String name, float fee) {
+		this.fee = fee;
+		this.name = name;
+		this.rollno = rollno;
+	}
+
+	void disply() {
+		System.out.println(name + " " + rollno + " " + fee);
+	}
+}
+
+public class This {
+	public static void main(String[] args) {
+		student s = new student(200, "Dhruv", 15000);
+		student s1 = new student(300, "Dk", 15000);
+		s.disply();
+		s1.disply();
+	}
+}
