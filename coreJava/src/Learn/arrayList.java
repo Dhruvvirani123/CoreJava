e718691e5a38273a1378f61d5f615098b1c47507
C <viranidhruv002@gmail.com>
2023-06-08 08:59:29 +0530

    Done

--- coreJava/src/Learn/arrayList.java
+++ coreJava/src/Learn/arrayList.java
@@ -1,6 +1,9 @@
 package Learn;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
 
 public class arrayList {
 	public static void main(String[] args) {
@@ -21,6 +24,54 @@ public class arrayList {
 		c.add(12.2f);
 		c.add(13.35f);
 		System.out.println(c);
+
+		Iterator itr = c.iterator();
+		while (itr.hasNext()) {
+			System.out.println(itr.next());
+		}
+
+		for (Float fl : c) { // for-each loop Iterator
+			System.out.println(fl);
+		}
+
+		ArrayList<String> abc = new ArrayList<String>();
+		abc.add("Dhruv");
+		abc.add("DK");
+		abc.add("Tirth");
+		System.out.println(abc);
+		System.out.println("Returning element : " + abc.get(2));// To get index number vise
+
+		for (String st : abc) {
+			System.out.println(st);
+		}
+
+		// Sort ArrayList
+
+		List<String> li = new ArrayList<String>();
+		li.add("Dhruv");
+		li.add("Romilbhai");
+		li.add("Rushit");
+		li.add("Bhargavbhai");
+		Collections.sort(li); // Alphabet Sorted
+		for (String s : li) {
+			System.out.println(s);
+		}
+		System.out.println("Sorting String...");
+
+		// Sort Integer
+
+		ArrayList<Integer> in = new ArrayList<Integer>();
+		in.add(12);
+		in.add(10);
+		in.add(90);
+		in.add(5);
+
+		Collections.sort(in); //Number vise sorted
+		for (Integer s1 : in) {
+			System.out.println(s1);
+		}
+		
+		
 
 	}
 }
