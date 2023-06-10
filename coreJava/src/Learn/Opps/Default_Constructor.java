3e649e6fd36d287a3ca9d14f41a98da0f39ca260
C <viranidhruv002@gmail.com>
2023-06-10 06:12:47 +0530

    Constructor

---
+++ coreJava/src/Learn/Opps/Default_Constructor.java
@@ -0,0 +1,13 @@
+package Learn.Opps;
+
+class dnv {
+	dnv() {
+		System.out.println("Unboxing...");
+	}
+}
+
+public class Default_Constructor {
+	public static void main(String[] args) {
+		dnv dnv = new dnv();
+	}
+}
