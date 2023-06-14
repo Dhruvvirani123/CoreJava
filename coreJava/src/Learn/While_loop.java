7ee337faa190063cff2370b9689dce110b530edb
C <viranidhruv002@gmail.com>
2023-06-14 10:29:35 +0530

    Done

--- coreJava/src/Learn/While_loop.java
+++ coreJava/src/Learn/While_loop.java
@@ -2,10 +2,17 @@ package Learn;
 
 public class While_loop {
 	public static void main(String[] args) {
-		int i = 0;
+		int i = 1;
 		while (i <= 10) {
 			System.out.println(i);
-			i = i + 2;
+			i = i + 1;
+		}
+
+		int year = 2020;
+		if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
+			System.out.println("Leap Year...");
+		} else {
+			System.out.println("Common Year");
 		}
 	}
 }
