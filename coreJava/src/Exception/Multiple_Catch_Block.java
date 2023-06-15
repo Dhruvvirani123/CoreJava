b486c136cb55a81bbbb96b03434873bb799e2ad5
C <viranidhruv002@gmail.com>
2023-06-15 16:07:09 +0530

    Done

---
+++ coreJava/src/Exception/Multiple_Catch_Block.java
@@ -0,0 +1,18 @@
+package Exception;
+
+public class Multiple_Catch_Block {
+	public static void main(String[] args) {
+		try {
+			int a[] = new int[5];
+			a[5] = 30 / 0;
+		} catch (ArithmeticException e) {
+			System.out.println("Arithmetic Exception occurs...");
+		}
+		catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("ArrayIndexOutOfBoundsException occurs...");
+		}
+		catch (Exception e) {
+			System.out.println("ArrayIndexOutOfBoundsException occurs...");
+		}
+	}
+}
--- coreJava/src/Learn/While_loop.java
+++ coreJava/src/Learn/While_loop.java
@@ -1,14 +1,18 @@
 package Learn;
 
+import java.util.Scanner;
+
 public class While_loop {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		int i = 1;
 		while (i <= 10) {
 			System.out.println(i);
 			i = i + 1;
 		}
 
-		int year = 2020;
+		System.out.print("Enter Any Year : ");
+		int year = sc.nextInt();
 		if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
 			System.out.println("Leap Year...");
 		} else {
