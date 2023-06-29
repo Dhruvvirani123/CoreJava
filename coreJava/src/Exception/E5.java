9e6c846f1f81b02fb31aa282b73d1d6d9afe863f
C <viranidhruv002@gmail.com>
2023-06-29 22:21:54 +0530

    Done

--- coreJava/src/Exception/E5.java
+++ coreJava/src/Exception/E5.java
@@ -3,11 +3,10 @@ package Exception;
 public class E5 {
 	public static void main(String[] args) {
 		try {
-			int data = 50/0;
-		}
-		catch (Exception e) {
+			int data = 50 / 0;
+		} catch (Exception e) {
 			System.out.println(e);
-			//System.out.println("Can't Dividedd by Zero");
+			// System.out.println("Can't Dividedd by Zero");
 		}
 		System.out.println("Can't Dividedd by Zero");
 	}
