258dedc8ab9be04f628ff1652a11de1436b82c81
C <viranidhruv002@gmail.com>
2023-07-01 09:23:36 +0530

    Done

---
+++ coreJava/src/Star_Patterns/P1.java
@@ -0,0 +1,12 @@
+package Star_Patterns;
+
+public class P1 {
+	public static void main(String[] args) {
+		for (int i = 0; i <= 6; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
---
+++ coreJava/src/Star_Patterns/P2.java
@@ -0,0 +1,15 @@
+package Star_Patterns;
+
+public class P2 {
+	public static void main(String[] args) {
+		for (int i = 0; i < 6; i++) {
+			for (int j = 2 * (6 - i); j >= 0; j--) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j <= i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}
