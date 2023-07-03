570dd3cae0467ad4a2551098d89c70a14008b92f
C <viranidhruv002@gmail.com>
2023-07-03 09:26:04 +0530

    Done

--- JSP_Dhruv/src/main/java/com/servlets/Firstservlet.java
+++ JSP_Dhruv/src/main/java/com/servlets/Firstservlet.java
@@ -1,5 +1,40 @@
 package com.servlets;
 
-public class Firstservlet implements {
+import java.io.IOException;
 
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class Firstservlet implements Servlet {
+
+	@Override
+	public void destroy() {
+		System.out.println("Going to destory servlet object...");
+	}
+
+	@Override
+	public ServletConfig getServletConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getServletInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("Creating Object...");
+
+	}
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		System.out.println("Servicing.....");
+	}
 }
